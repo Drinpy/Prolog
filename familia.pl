565e38ff763cceaad73eeb92018f8bb8db98a0e2
Pedro Henrique Borges Luiz <66181571+Drinpy@users.noreply.github.com>
2023-05-30 23:23:46 -0300

    Create familia.pl

---
+++ familia.pl
@@ -0,0 +1,16 @@
+pai(gabriel, gael).
+pai(pedro, natalia).
+pai(ryan, diniz).
+pai(amano, vitoria).
+pai(gael, andre).
+pai(diniz, diane).
+pai(andre, arena).
+
+mae(leticia, natalia).
+mae(jade, gael).
+mae(ana, diniz).
+mae(patricia, vitoria).
+mae(natalia, andre).
+mae(vitoria, diane).
+mae(diane, arena).
+
