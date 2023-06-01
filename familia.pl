f9ad50f45f5c8f11a146700ddcba6041023c3dcb
Pedro Henrique Borges Luiz <66181571+Drinpy@users.noreply.github.com>
2023-06-01 17:22:56 -0300

    Update familia.pl

--- familia.pl
+++ familia.pl
@@ -1,15 +1,46 @@
 pai(gabriel, gael).
 pai(pedro, natalia).
 pai(ryan, diniz).
-pai(amano, vitoria).
+pai(amano, mariane).
 pai(gael, andre).
+pai(gael, neves).
 pai(diniz, diane).
 pai(andre, arena).
+pai(neves, claudinei).
+pai(jeremias, julia).
+pai(afonso, jeremias).
+pai(sergio, aurora).
 
-mae(jade, gael).
 mae(leticia, natalia).
+mae(jade, gael).
 mae(ana, diniz).
-mae(patricia, vitoria).
+mae(patricia, mariane).
 mae(natalia, andre).
-mae(vitoria, diane).
+mae(natalia, neves).
+mae(mariane, diane).
 mae(diane, arena).
+mae(julia, claudinei).
+mae(aurora, julia).
+mae(isis, jeremias).
+mae(aline, aurora).
+
+irmao(X,Y) :-mae(Z,X),mae(Z,Y),X \= Y.
+
+avoH(Z,Y):-pai(X,Y),pai(Z,X).
+avoH(Z,Y):-mae(X,Y),pai(Z,X).
+
+avoM(Z,Y):-mae(X,Y),mae(Z,X).
+avoM(Z,Y):-pai(X,Y),mae(Z,X).
+
+neto(Z,Y):-avoH(Y,Z).
+neto(Z,Y):-avoM(Y,Z).
+
+tio(Z,Y):-irmao(Z,X),pai(X,Y).
+
+primo(Z,Y):-tio(X,Y),pai(X,Z).
+
+
+
+
+
+
